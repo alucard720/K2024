dec76b7bfb9003cfebe5c49c655c1277e01c0656
alucard720 <ing.santana36@gmail.com>
2023-10-05 15:12:59 -0400

    laptop06

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const bycrpt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 const sql = require("mssql");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
@@ -12,8 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: false }));
 //start sql_server
 const config = {
   user: process.env.SQL_USER,
@@ -43,18 +42,26 @@ app.get("/mssql/users", async function (req, res) {
   res.json({ msg: "Fetch user success", data: result.recordsets });
 });
 
+async function hashPassword(password) {
+  const saltRounds = 10;
+  return bcrypt.hash(password, saltRounds);
+}
+
 //POST register
 app.post("/mssql/register", async function (req, res) {
-  const { username, password } = req.body;
-  const request = db.request();
-  const hashedpassword = await bycrpt.hash(password, 10);
-
-  const query =
-    "insert into users(username, password) values(@username, @password)";
-  request
-    .input("username", sql.NVarChar(50), username)
-    .input("password", sql.NVarChar(50), password.hashedpassword);
+  const { email, password } = req.body;
   try {
+    const hashedPassword = await hashPassword(password);
+
+    //connect to database to save user with hash password
+    const request = db.request();
+    const query =
+      "insert into users(email, password) values(@email, @password)";
+    request
+      .input("email", sql.NVarChar, email)
+      .input("password", sql.VarChar, hashedPassword);
+
+    //results
     const result = await request.query(query);
     console.log(result);
     res.json({ success: true, msg: "Save User Data successfully" });
@@ -63,49 +70,58 @@ app.post("/mssql/register", async function (req, res) {
     res.status(500).json({ success: false, message: "Registration failed" });
   }
 });
-
 //POST login
 
 app.post("/mssql/login", async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
-  if (!username || !password) {
-    return res
-      .status(400)
-      .json({ message: false, message: "username and password requeridos" });
+  if (!email || !password) {
+    throw new Error("Please enter valid email and password");
   }
 
   try {
-    const request = db.request();
+    await sql.connect(config);
+    const result =
+      await sql.query`SELECT email, password FROM users WHERE email = ${email}`;
 
-    const query =
-      "SELECT username, password FROM users WHERE username = @username ";
+    const user = result.recordset[0];
+    if (!user || !(await correctPassword(password, user.password))) {
+      throw new Error("Incorrect email or password");
+    }
+    if (result.recordset.length > 0) {
+      res.status(200).json({ message: "Login successful" });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  } finally {
+    sql.close();
+  }
 
-    request.input("username", sql.NVarChar(50), username);
-    /* .input("password", sql.NVarChar(50), password); */
-
-    const result = await request.query(query);
+  /* 
+  try {
+    const result = await db.query(query);
     if (result.recordsets.length === 1) {
       const user = result.recordsets[0];
+      const match = await bycrpt.compare(password, user.password);
 
-      console.log(user);
-
-      if (user.password === password) {
-        res.status(200).json({ message: "login successful" });
-      }
-
-      /* const match = await bycrpt.compare(password, user.password); */
-
-      /*   if (match) {
-        res.json({ message: true, message: "Login succesful" });
+      if (match) {
+        const token = jwt.sign({ email: user.email }, process.env.SECRET_KEY, {
+          expiresIn: "1h",
+        });
+        res.json({ success: true, token: token });
       } else {
-        res.json({ message: false, message: "Login failed" });
-      } */
+        res.status(401).json({ success: false, status: "Invalid Credentials" });
+      }
+    } else {
+      res.status(401).json({ success: false, status: "User not Found" });
     }
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: "Login failed" });
-  }
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  } */
 });
 
 //POST UPDATE user
@@ -131,6 +147,10 @@ app.post("/mssql/delete:id", async (req, res) => {
   await request.query("delete * from users id=@id");
   res.json({ msg: "Delete User Data successfully" });
 });
+
+async function correctPassword(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+}
 // start the server
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
